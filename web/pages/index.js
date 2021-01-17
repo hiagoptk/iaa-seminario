2217bf71b10ca5cac96cb754e1fdddfc058b5bf3
Hiago Patrick <hiogaptk@gmail.com>
2021-01-17 15:19:15 -0300

    Repassando antigos arquivos

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,65 +1,39 @@
+import React from 'react'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+const Home = () => {
+    return (
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+       
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <>
+             <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            
+            <Navbar />
+                
+            <div id='main'>
+                <div className='box' >
+                    <p>Imersões: arte e arquitetura é um seminário internacional realizado pela Escola Sem Sítio, em parceria com a Casa França Brasil e os grupos de extensão CURA e Linda, respectivamente vinculados a UERJ e a PUC-Rio, que acontecerá online, com transmissão ao vivo pelo Youtube, entre os dias 2 e 5 de março de 2021. Premiado no edital #RetomadaCulturalRJ da Secretaria de Estado de Cultura e de Economia Criativa do Rio de Janeiro e parte integrante da programação paralela do  27o Congresso Mundial de Arquitetos - UIA 2021 no ano em que o Rio recebe da UNESCO o título de Capital Mundial da Arquitetura, Imersões se faz em uma zona de convergências entre as práticas artísticas, arquitetônicas, urbanísticas e educacionais. </p>
+                    
+                    <p>Seu ciclo de debates promoverá reflexões que transitam nesses campos do saber, promovendo sua difusão, ao mesmo tempo em que tornam a arte contemporânea e a arquitetura responsáveis por mais compromissos sociais. Como pensar as relações entre arte, arquitetura, a humanidade e a habitação social? A quem a arquitetura se destina? A quem, de fato, atende? Quais questões permeiam e afetam, sobretudo, a vida urbana? Que questionamentos podem ser trazidos para esse campo? O que se vislumbra? </p>
+                    
+                    <p>Confira a programação completa aqui. A partir do dia x estarão abertas as inscrições para submissão de trabalhos em vídeo que poderão ser exibidos durante as mesas de debate. A inscrição para assistir às mesas terá início no dia x.</p>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+                    <div id='sub'>
+                        <h3>Em breve!</h3>
+                        <button> SUBMETER TRABALHO </button>
+                    </div>
+                </div>
+              
+            </div>
+        </>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    )
 }
+
+export default Home;
---
+++ web/pages/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+
+export const Main = styled.main`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;  
+  align-items: center;
+  justify-content: center;
+  
+  border: blue 1px solid;
+`;
