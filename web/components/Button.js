6a4b07b3e61c6f83b1f6f5cb60ddf70c8f342b9a
Hiago Patrick <hiogaptk@gmail.com>
2021-01-25 20:54:42 -0300

    adc arquivos, alterando links e correção faq

--- web/components/Button.js
+++ web/components/Button.js
@@ -3,19 +3,14 @@ import React from 'react'
 const Button = () => {
     return ( 
         <>
-                <button className='button' id='pdfConvocatory'>
+            <button className='button' id='pdfConvocatory'>
                 <a href='https://drive.google.com/file/d/1ZYlrC7TAX4_Y2zxCmbTuRPLUNTrLNs-n/view?usp=sharing' target='_blank'> CONVOCATÓRIA </a>
-                </button>
-            
-
-
-            
-                <button className='button' id='pdfFAQ' >
-                 <a href='https://drive.google.com/file/d/1E3YY_PtWHXrgv4JQK3OTmNbDg0NdDwez/view?usp=sharing' target='_blank'>  FAQ </a>
-                </button>
-            
-
-
+            </button>
+   
+            <button className='button' id='pdfFAQ' >
+                <a href='https://drive.google.com/file/d/1-j-MD2IX7JeRcPIXPJOTpumWKM0UUJLk/view?usp=sharing' target='_blank'>  FAQ </a>
+            </button>
+   
             <a href='https://docs.google.com/forms/d/1qF_8qfsI5sj6Z6Bu9Xr2czZNMzVhAExqQS7VtdV9QZE/edit' target='_blank'>
                 <button className='button' id='form' disabled>
                     INSCREVA-SE
