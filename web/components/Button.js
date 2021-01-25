18b274cf1498112be861c28c1a5ffd627971aa4e
Hiago Patrick <hiogaptk@gmail.com>
2021-01-25 20:36:42 -0300

    aplicando mãosinha convocatória

--- web/components/Button.js
+++ web/components/Button.js
@@ -3,18 +3,17 @@ import React from 'react'
 const Button = () => {
     return ( 
         <>
-            <a href='https://drive.google.com/file/d/1ZYlrC7TAX4_Y2zxCmbTuRPLUNTrLNs-n/view?usp=sharing' target='_blank'>
                 <button className='button' id='pdfConvocatory'>
-                    CONVOCATÓRIA 
+                <a href='https://drive.google.com/file/d/1ZYlrC7TAX4_Y2zxCmbTuRPLUNTrLNs-n/view?usp=sharing' target='_blank'> CONVOCATÓRIA </a>
                 </button>
-            </a>
+            
 
 
-            <a href='https://drive.google.com/file/d/1E3YY_PtWHXrgv4JQK3OTmNbDg0NdDwez/view?usp=sharing' target='_blank'>
+            
                 <button className='button' id='pdfFAQ' >
-                    FAQ 
+                 <a href='https://drive.google.com/file/d/1E3YY_PtWHXrgv4JQK3OTmNbDg0NdDwez/view?usp=sharing' target='_blank'>  FAQ </a>
                 </button>
-            </a>
+            
 
 
             <a href='https://docs.google.com/forms/d/1qF_8qfsI5sj6Z6Bu9Xr2czZNMzVhAExqQS7VtdV9QZE/edit' target='_blank'>
