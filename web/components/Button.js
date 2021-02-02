6b789541dee519261ec757eb0c80db48cb39262d
Hiago Patrick <hiogaptk@gmail.com>
2021-02-02 00:18:39 -0300

    liberando formulário de inscrição

--- web/components/Button.js
+++ web/components/Button.js
@@ -12,7 +12,7 @@ const Button = () => {
             </button>
    
             <a href='https://docs.google.com/forms/d/1qF_8qfsI5sj6Z6Bu9Xr2czZNMzVhAExqQS7VtdV9QZE/edit' target='_blank'>
-                <button className='button' id='form' disabled>
+                <button className='button' id='form' >
                     INSCREVA-SE
                 </button>
             </a>
