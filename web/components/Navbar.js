4bc5ae3f26e2b9e9ae8ef500bf2e029a1d96f8d2
Hiago Patrick <hiogaptk@gmail.com>
2021-02-02 23:05:04 -0300

    alterando menu de navegação

--- web/components/Navbar.js
+++ web/components/Navbar.js
@@ -26,7 +26,7 @@ const Navbar = () => {
                     <li> <p>*</p> </li>
                     <li> <a href='/programacao'>PROGRAMAÇÃO</a></li>
                     <li> <p>*</p> </li>
-                    <li> <a href=''>PALESTRANTES</a></li>
+                    <li> <a id='disabled'>PALESTRANTES</a></li>
                     <li> <p>*</p> </li>
                     <li> <a href='/contato'>CONTATO  </a></li>
                 </ul>
