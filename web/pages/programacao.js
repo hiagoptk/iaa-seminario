c398c9b1ff22e42f3cbb32913e264c2aee70f85f
Hiago Patrick <63915307+hiagoptk@users.noreply.github.com>
2021-02-08 16:54:00 -0300

    Update programacao.js

--- web/pages/programacao.js
+++ web/pages/programacao.js
@@ -24,7 +24,7 @@ const Contato = () => {
                 <div className='box' id='topoPrograma' >
                   
                    <p>Todas as mesas serão transmitidas ao vivo pela Internet e terão tradução simultânea para Libras. Os participantes previamente inscritos receberão certificado. 
-                      Faça sua inscrição gratuita <b><a target='_blank'  href='https://docs.google.com/forms/d/1qF_8qfsI5sj6Z6Bu9Xr2czZNMzVhAExqQS7VtdV9QZE/viewform?edit_requested=true'>AQUI</a></b></p>
+                      Faça sua <b><a target='_blank'  href=''>inscrição</a></b>  gratuita.</p>
                 </div>
 
                 
