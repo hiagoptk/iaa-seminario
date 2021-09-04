41de5782498cf87bce4e5e8f7eb902663ea72812
Hiago Patrick <63915307+hiagoptk@users.noreply.github.com>
2021-09-04 00:41:10 -0300

    Update programacao.js
    correção de bug no vídeo do dia 3

--- web/pages/programacao.js
+++ web/pages/programacao.js
@@ -117,7 +117,7 @@ const Contato = () => {
                             
                              <iframe width="560" 
                                         height="315" 
-                                        src="https://www.youtube.com/watch?v=p2NDwLrKz34" 
+                                        src="https://www.youtube.com/embed/p2NDwLrKz34" 
                                         title="YouTube video player" 
                                         frameborder="0" 
                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
