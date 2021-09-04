68b451edc25f145649a8ccc557b161f5bad28620
Hiago Patrick <63915307+hiagoptk@users.noreply.github.com>
2021-09-04 00:28:27 -0300

    Update convocatoria.js
    correção de estilo

--- web/pages/convocatoria.js
+++ web/pages/convocatoria.js
@@ -26,7 +26,7 @@ const Convocatory = () => {
                 <div className='box' >
         
                     <p>A presente chamada propôs a seleção de apresentações em vídeo de até 4’59” (quatro minutos de cinquenta e nove segundos) de duração com vivências, reflexões, 
-                       impressões, práticas artísticas, urbanísticas e arquitetônicas que possam contribuiram para as discussões empreendidas pelo Seminário Imersões: arte e arquitetura, 
+                       impressões, práticas artísticas, urbanísticas e arquitetônicas que possam contribuiram para as discussões empreendidas pelo <b>Seminário Imersões: arte e arquitetura</b>, 
                        proporcionando outras interfaces ao ampliar o debate para além das falas das (dos) palestrantes. Foram experiências, oriundas do meio acadêmico, artístico e/ou 
                        de comunidades e movimentos sociais, que se colocam como insurgência ou alternativa às estruturas hegemônicas.</p>
                     
