e6f147dd78030fed9af370a34d1e7d71b7130b39
Hiago Patrick <63915307+hiagoptk@users.noreply.github.com>
2021-09-04 01:12:14 -0300

    Update Navbar.js
    Alterando os títulos da navbar

--- web/components/Navbar.js
+++ web/components/Navbar.js
@@ -24,11 +24,11 @@ const Navbar = () => {
                     <li> <p>*</p> </li>
                     <li> <a href='/convocatoria'>CONVOCATÓRIA</a></li>
                     <li> <p>*</p> </li>
-                    <li> <a href='/programacao'>PROGRAMAÇÃO</a></li>
+                    <li> <a href='/programacao'>PROGRAMAÇÃO (assista aqui aos vídeos)</a></li>
                     <li> <p>*</p> </li>
                     <li> <a href='/palestrantes'>PALESTRANTES</a></li>
                     <li> <p>*</p> </li>
-                    <li> <a href='/contato'>CONTATO  </a></li>
+                    <li> <a href='/contato'>SOBRE O PROJETO</a></li>
                 </ul>
             </nav>
     )
