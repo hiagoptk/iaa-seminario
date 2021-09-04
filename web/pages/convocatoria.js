5a527decc6b34ad432a8baa89e8f1fe2fa2ed920
Hiago Patrick <63915307+hiagoptk@users.noreply.github.com>
2021-09-04 01:56:28 -0300

    Update convocatoria.js

--- web/pages/convocatoria.js
+++ web/pages/convocatoria.js
@@ -23,16 +23,17 @@ const Convocatory = () => {
                     <BannerConvocatory />
                 </div> 
                 
+                      
                 <div className='box' >
-                    <p>A presente chamada propõe a seleção de apresentações em vídeo de até 4’59” (quatro minutos de cinquenta e nove segundos)
-                       de duração com vivências, reflexões, impressões, práticas artísticas, urbanísticas e arquitetônicas que possam contribuir 
-                       para as discussões empreendidas pelo <b>Seminário Imersões: arte e arquitetura</b>, proporcionando outras interfaces ao ampliar o 
-                       debate para além das falas das (dos) palestrantes. Interessa ao projeto apresentar experiências, oriundas do meio acadêmico, 
-                       artístico e/ou de comunidades e movimentos sociais, que se coloquem como insurgência ou alternativa às estruturas hegemônicas. </p>
+        
+                    <p>A presente chamada propôs a seleção de apresentações em vídeo de até 4’59” (quatro minutos de cinquenta e nove segundos) de duração com vivências, reflexões, 
+                       impressões, práticas artísticas, urbanísticas e arquitetônicas que possam contribuiram para as discussões empreendidas pelo <b>Seminário Imersões: arte e arquitetura</b>, 
+                       proporcionando outras interfaces ao ampliar o debate para além das falas das (dos) palestrantes. Foram experiências, oriundas do meio acadêmico, artístico e/ou 
+                       de comunidades e movimentos sociais, que se colocam como insurgência ou alternativa às estruturas hegemônicas.</p>
                     
-                    <p><b>O prazo para submissão de trabalhos encerrou-se em 12 de fevereiro. O resultado está divulgado nesta página</b></p>
+                    <p><b>O prazo para submissão de trabalhos encerrou-se em 12 de fevereiro. O resultado foi divulgado nesta página</b></p>
                 </div>
-              
+                  
                 <div id='buttonBox'>
                     <Button />
                 </div>
