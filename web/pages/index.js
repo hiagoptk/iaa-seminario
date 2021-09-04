b561e2a0d224bebe10e4275a3d800099c6cb87d0
Hiago Patrick <63915307+hiagoptk@users.noreply.github.com>
2021-09-04 01:30:36 -0300

    Update index.js

--- web/pages/index.js
+++ web/pages/index.js
@@ -28,32 +28,24 @@ const Home = () => {
                 </div> 
 
                 <div className='box' >
-                    <p> <b>Imersões: arte e arquitetura</b> é um seminário internacional on-line que propõe uma 
-                        reflexão sobre os papéis da arquitetura e urbanismo na contemporaneidade e sua 
-                        intercessão e interface com a arte. Com transmissão ao vivo pela Internet entre os 
-                        dias 2 e 5 de março de 2021, seu ciclo de debates promoverá reflexões que transitam 
-                        nesses campos do saber, promovendo sua difusão, ao mesmo tempo em que tornam a 
-                        arte contemporânea e a arquitetura responsáveis por mais compromissos sociais. </p>
-                    
-                    <p> Os eixos centrais de <b>Imersões</b> – Arte e arquitetura, Habitação social e Arquitetura 
-                        e alteridades – procuram ampliar a discussão que se apresenta urgente e desdobra, no 
-                        ano em que o Rio recebe da UNESCO o título de Capital Mundial da Arquitetura, o mote 
-                        anunciado pelo 27o Congresso Mundial de Arquitetos – UIA 2021: <i>Todos os mundos. <b>Um só 
-                        mundo</b></i>.</p>
-                    
-                    <p> Os palestrantes trarão experiências que abordem a configuração do espaço urbano como 
-                        oportunidade de contato e exercício cotidiano da cidade e da arte, discutindo 
-                        transitoriedade, fluxos e outras formas de viver. Como pensar as relações entre arte, 
-                          arquitetura, a humanidade e a habitação social? A quem a arquitetura se destina? A quem, 
-                        de fato, atende? Quais questões permeiam e afetam, sobretudo, a vida urbana? Que 
-                        questionamentos podem ser trazidos para esse campo? O que se vislumbra? </p>
-
-                    <p> Premiado no edital #RetomadaCulturalRJ da Secretaria de Estado de Cultura e de 
-                        Economia Criativa do Rio de Janeiro e parte integrante do calendário oficial de eventos 
-                        culturais do UIA 2021, “Imersões” é uma realização em parceria da Escola Sem Sítio, 
-                        Casa França-Brasil e dos grupos de extensão e pesquisa CURA/UERJ e Linda/PUC-Rio.</p>
-
-                   
+        
+                    <p><b>Imersões: arte e arquitetura</b> foi um seminário internacional on-line que propôs uma reflexão sobre os papéis da arquitetura e urbanismo na 
+                        contemporaneidade e sua intercessão e interface com a arte. Com transmissão ao vivo pela Internet entre os dias 2 e 5 de março de 2021, seu ciclo 
+                        de debates trouxe discussões que transitam nesses campos do saber, promovendo sua difusão, ao mesmo tempo em que tornam a arte contemporânea e a 
+                        arquitetura responsáveis por mais compromissos sociais. </p>
+        
+                    <p>Os eixos centrais de <b>Imersões</b> – Arte e arquitetura, Habitação social e Arquitetura e alteridades – procuram ampliar a discussão que se apresenta 
+                       Mundial de Arquitetos – UIA 2021: Todos os mundos. <b><i>Um só mundo</i></b>.</p>
+        
+                    <p>Os palestrantes trouxeram experiências que abordam a configuração do espaço urbano como oportunidade de contato e exercício cotidiano da cidade e da arte, 
+                       discutindo transitoriedade, fluxos e outras formas de viver. Como pensar as relações entre arte, arquitetura, a humanidade e a habitação social? A quem a 
+                       arquitetura se destina? A quem, de fato, atende? Quais questões permeiam e afetam, sobretudo, a vida urbana? Que questionamentos podem ser trazidos para 
+                       esse campo? O que se vislumbra? </p>
+        
+                    <p>Premiado no edital #RetomadaCulturalRJ da Secretaria de Estado de Cultura e de Economia Criativa do Rio de Janeiro e parte integrante do calendário oficial 
+                       de eventos culturais do UIA 2021, “Imersões” foi uma realização em parceria da Escola Sem Sítio, Casa França-Brasil e dos grupos de extensão e 
+                       pesquisa CURA/UERJ e Linda/PUC-Rio.</p>
+                  
                    <div className='boxImg'>
                         <a target='_blank' href='https://www.even3.com.br/seminarioimersoes/' >
                             <HomeImg />
