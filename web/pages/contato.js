15b8ff6e24c6a77509de5348e4d8afe274a24951
Hiago Patrick <63915307+hiagoptk@users.noreply.github.com>
2021-09-04 00:50:07 -0300

    Update contato.js
    Teste página contato

--- web/pages/contato.js
+++ web/pages/contato.js
@@ -22,14 +22,15 @@ const Contato = () => {
                 </div> 
                 
                 <div className='box' id='boxContato' >
-                    <p>Dúvidas sobre a <b><a href='https://drive.google.com/file/d/1ZYlrC7TAX4_Y2zxCmbTuRPLUNTrLNs-n/view?usp=sharing' target='_blank'>Convocatória</a></b> poderão ser dirimidas por meio do e-mail. 
-                        Para um suporte adequado, somente serão respondidas questões relativas à interpretação 
-                        da convocatória e que não estejam previstas no <b><a href='https://drive.google.com/file/d/1E3YY_PtWHXrgv4JQK3OTmNbDg0NdDwez/view?usp=sharing' target='_blank'>FAQ</a></b>.</p>
-                   <div className='boxContatos'>
-                    <p><span>ORGANIZAÇÃO</span> <br /> <b>seminario@imersoes.arq.br</b></p>
-                        
-                    <p><span>ASSESSORIA DE IMPRENSA</span> <br /> <b>carolina.feital@trevocomunicativa.com.br</b></p> 
-                    </div>
+                   <p>
+                       SEMINÁRIO <br>
+                       ORGANIZAÇÃO e CURADORIA<br>
+
+                        André Carvalho<br>
+                        Jocelino Pessoa<br>
+                        Marcelo Campos<br>
+                        Tania Queiroz<br>
+                   </p>
                 </div>
               
             </div>
