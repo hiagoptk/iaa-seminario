d9714002c860e35a1666983aa606afd484de085c
hiagoptk <hiogaptk@gmail.com>
2021-09-08 21:47:01 -0300

    correção no css dos vídeos na aba VIDEOS

--- web/pages/programacao.js
+++ web/pages/programacao.js
@@ -60,7 +60,7 @@ const Contato = () => {
                                         title="YouTube video player" 
                                         frameborder="0" 
                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                                        allowfullscreen></iframe>
+                                        allowfullscreen></iframe> 
                     </div>
 
 
