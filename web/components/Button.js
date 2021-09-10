667938e9231e98dae37a513497d98ac3108d3401
hiagoptk <hiogaptk@gmail.com>
2021-09-09 21:52:57 -0300

    tentativa 4

--- web/components/Button.js
+++ web/components/Button.js
@@ -7,8 +7,8 @@ const Button = () => {
                 <a href='https://drive.google.com/file/d/1ZYlrC7TAX4_Y2zxCmbTuRPLUNTrLNs-n/view?usp=sharing' target='_blank'> CONVOCATÓRIA </a>
             </button>
    
-            <button className='button' id='pdfFAQ' >
-                <a href='/public/docs/FAQ-final.pdf' download="FAQ_Imersões" target='_blank'>  FAQ </a>
+            <button className='button' type="submit" onclick="window.open('FAQ-final.pdf')" id='pdfFAQ' >
+                <a href='/public/docs/FAQ-final.pdf' target='_blank'>  FAQ </a>
             </button>
    
             
